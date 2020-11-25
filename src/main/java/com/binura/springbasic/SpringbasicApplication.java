d589a4fc8e4a5758120b08e47ff87226c64e05d9
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2020-11-26 01:06:54 +0530

    initial

---
+++ src/main/java/com/binura/springbasic/SpringbasicApplication.java
@@ -0,0 +1,14 @@
+package com.binura.springbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbasicApplication.class, args);
+        System.out.println("Hello world");
+    }
+
+}
