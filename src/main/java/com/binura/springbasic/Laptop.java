55ac71ebac18185e1721146e0a5733a3f42c5a8b
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2020-11-26 01:46:34 +0530

    autowired

---
+++ src/main/java/com/binura/springbasic/Laptop.java
@@ -0,0 +1,12 @@
+package com.binura.springbasic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+    public void Compile() {
+        System.out.println("hellow");
+    }
+}
--- src/main/java/com/binura/springbasic/SpringbasicApplication.java
+++ src/main/java/com/binura/springbasic/SpringbasicApplication.java
@@ -2,13 +2,16 @@ package com.binura.springbasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringbasicApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbasicApplication.class, args);
+        ApplicationContext context = SpringApplication.run(SpringbasicApplication.class, args);
         System.out.println("Hello world");
+        Alien obj = context.getBean(Alien.class);
+        obj.code();
     }
 
 }
